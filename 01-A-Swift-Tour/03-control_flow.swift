138e1a47ba603b06d996a12996ef4172bee2aeef
mizokami <suzunatsu@yahoo.com>
2014-10-29 05:33:44 +0900

    case文 where は癖がある感じがするけど、まあ便利そうかな
    ruby だと
    
    ```rb
    vegitable = "red pepper"
    case vegitable
    when "celery"
      # ...
    when "cucumber", "watercress"
      # ...
    when /pepper\z/
      # ...
    end
    ```
    
    みたいに正規表現使って書くしかない感じなので柔軟といえそう

--- 01-A-Swift-Tour/03-control_flow.swift
+++ 01-A-Swift-Tour/03-control_flow.swift
@@ -22,3 +22,22 @@ if let name = optionalName {
   greeting = "Hello, \(name)"
 }
 println(greeting)
+
+println("\n# page 13")
+
+let vegitable = "red pepper"
+var vegitableComment: String = "None"
+
+switch vegitable {
+case "celery":
+  vegitableComment = "Add some raisins and make ants on a log."
+case "cucumber", "watercress":
+  vegitableComment = "That would make a good tea sandwich."
+case let x where x.hasSuffix("pepper"):
+  vegitableComment = "Is it a spicy \(x)?"
+default:
+  vegitableComment = "Everything tastes good in soup."
+}
+
+println("vegitable is \(vegitable)")
+println("vegitableComment is \(vegitableComment)")
