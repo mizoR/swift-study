9d2e6e8015b82d73f8742ac67db7fc69599736d6
mizokami <suzunatsu@yahoo.com>
2014-10-29 04:59:23 +0900

    init

---
+++ 01-A-Swift-Tour/02-simple_values.swift
@@ -0,0 +1,41 @@
+println("# page 6")
+var myVariable = 42
+myVariable = 50
+
+let myConstant = 42
+
+println("myVariable is \(myVariable)")
+println("myConstant is \(myConstant)")
+
+println("\n# page 7")
+let implicitInteger = 70
+let implicitDouble = 70.0
+let explictDouble: Double = 70
+
+println("implicitInteger is \(implicitInteger)")
+println("implicitDouble is \(implicitDouble)")
+println("explictDouble is \(explictDouble)")
+
+let apples = 3
+let oranges = 5
+let appleSummary = "I have \(apples) apples."
+let fruitSummary = "I have \(apples + oranges) pieces of fruit."
+
+println("\n# page 8")
+println(appleSummary)
+println(fruitSummary)
+
+
+println("\n# page 9")
+
+var shoppingList = ["catfish", "water", "tulips", "blue paint"]
+shoppingList[1] = "bottle of water"
+
+var occupations = [
+  "Malcolm": "Captain",
+  "Kaylee": "Mechanic",
+]
+occupations["Janye"] = "Public Relations"
+
+println("shoppingList are \(shoppingList)")
+println("occupations are \(occupations)")
