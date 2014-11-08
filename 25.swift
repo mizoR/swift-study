97b12b186a39a679d335d1deed7f4c74b39755af
mizokami <suzunatsu@yahoo.com>
2014-11-08 16:51:21 +0900

    継承もかけたぞ
    親コンストラクタを呼ぶときに super.init() と書くのが個人的には妙な違和感を感じがけど、そういうものだと理解する

---
+++ 25.swift
@@ -0,0 +1,33 @@
+class NamedShape {
+  var numberOfSides: Int = 0
+    var name: String
+
+    init(name: String) {
+      self.name = name
+    }
+
+  func simpleDescription() -> String {
+    return "A shape with \(numberOfSides) sides."
+  }
+}
+
+class Squire: NamedShape {
+  var sideLength: Double
+
+  init(sideLength: Double, name: String) {
+    self.sideLength = sideLength
+    super.init(name: name)
+    numberOfSides = 4
+  }
+
+  func area() -> Double {
+    return sideLength * sideLength
+  }
+
+  override func simpleDescription() -> String {
+    return "A square with sides of length \(sideLength)."
+  }
+}
+let test = Squire(sideLength: 5.2, name: "my test squire")
+println("area = \(test.area())")
+println(test.simpleDescription())
