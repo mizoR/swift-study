59c2417a28140199a1d6aed6fe90544d2941bc27
mizokami <suzunatsu@yahoo.com>
2014-11-08 16:25:29 +0900

    関数を返す関数をかける
    この場合、戻り値の型は、返却する関数の引数と戻り値のセットになるらしい

--- 20.swift
+++ 20.swift
@@ -9,3 +9,14 @@ func returnFifteen() -> Int {
 
 let fifteen = returnFifteen()
 println("fifteen = \(fifteen)")
+
+func makeIncrementer() -> (Int -> Int) {
+  func addOne(number: Int) -> Int {
+    return 1 + number
+  }
+  return addOne
+}
+
+var increment = makeIncrementer()
+let eight = increment(7)
+println("eight = \(eight)")
