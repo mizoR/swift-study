e97e26b786958ea78287339ff3468602cb643a86
mizokami <suzunatsu@yahoo.com>
2014-11-12 16:08:06 +0900

    enum 型 インスタンスメソッドも生やせて便利!!!
    ebook には toRaw() を使えって書いてあったけど、どうやらこのメソッドはなくなっているみたいで rawValue インスタンス変数を使うのが今は正しいっぽかった

---
+++ 30.swift
@@ -0,0 +1,32 @@
+enum Rank: Int {
+  case Ace = 1
+  case Tow, Three, Four, Five, Six, Seven, Eight, Nine, Ten
+  case Jack, Queen, King
+  func simpleDescription() -> String {
+    switch self {
+    case .Ace:
+      return "ace"
+    case .Jack:
+      return "jack"
+    case .Queen:
+      return "queen"
+    case .King:
+      return "king"
+    default:
+      return String(self.rawValue)
+    }
+  }
+}
+
+let ace = Rank.Ace
+
+println("                     ace = \(ace)")
+println(" ace.simpleDescription() = \(ace.simpleDescription())")
+println("            ace.rawValue = \(ace.rawValue)")
+
+let four = Rank.Four
+
+println("                    four = \(four)")
+println("four.simpleDescription() = \(four.simpleDescription())")
+println("           four.rawValue = \(four.rawValue)")
+
