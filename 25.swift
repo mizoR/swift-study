c7b27dac76b59e1b0a10a6ac6e2ef6ab27ed2d89
mizokami <suzunatsu@yahoo.com>
2014-11-12 15:37:56 +0900

    クラスメソッドに setter と getter を追加出来るようになった!! setter において与えられた値は newValue で取得できる!!!

--- 25.swift
+++ 25.swift
@@ -32,3 +32,33 @@ class Square: NamedShape {
 let square = Square(sideLength: 5.2, name: "my squire")
 println("area = \(square.area())")
 println(square.simpleDescription())
+
+class EquilateralTriangle: NamedShape {
+  var sideLength: Double = 0.0
+
+    init(sideLength: Double, name: String) {
+      self.sideLength = sideLength
+        super.init(name: name)
+        numberOfSides = 3
+    }
+
+  var perimeter: Double {
+    get {
+      return 3.0 * sideLength
+    }
+    set {
+      sideLength = newValue / 3.0
+    }
+  }
+
+  override func simpleDescription() -> String {
+    return "An equilateral triangle with sides of length \(sideLength)."
+  }
+}
+
+var triangle = EquilateralTriangle(sideLength: 3.1, name: "a triangle")
+
+println("perimeter = \(triangle.perimeter)")
+
+triangle.perimeter = 0.9
+println(triangle.simpleDescription())
