740f68bcabd8d7cab32a40f764835677d6568e2d
mizokami <suzunatsu@yahoo.com>
2014-11-13 15:38:30 +0900

    EXPERIMENT: black/red を返すメソッドを作る

--- 32.swift
+++ 32.swift
@@ -1,5 +1,6 @@
 enum Suit {
   case Spades, Hearts, Diamonds, Clubs
+
   func simpleDescription() -> String {
     switch self {
       case .Spades:
@@ -12,8 +13,20 @@ enum Suit {
         return "clubs"
     }
   }
+
+  func color() -> String {
+    switch self {
+      case .Spades, .Clubs:
+        return "black"
+      case .Hearts, .Diamonds:
+        return "red"
+    }
+  }
 }
 
 let hearts = Suit.Hearts
 let heartsDescription = hearts.simpleDescription()
+let heartsColor = hearts.color()
+
 println(heartsDescription)
+println(heartsColor)
