dd1220be0098ed41f2c3125d8e8184c443d3038e
mizokami <suzunatsu@yahoo.com>
2014-11-13 15:43:28 +0900

    enum 型の Rank と Suit をインスタンス変数に持つ Card 構造体を作りました

--- 30.swift
+++ 30.swift
@@ -35,3 +35,51 @@ if let convertedRank = Rank.init(rawValue: 3) {
 
   println(threeDescription)
 }
+
+enum Suit {
+  case Spades, Hearts, Diamonds, Clubs
+
+  func simpleDescription() -> String {
+    switch self {
+      case .Spades:
+        return "spades"
+      case .Hearts:
+        return "hearts"
+      case .Diamonds:
+        return "diamonds"
+      case .Clubs:
+        return "clubs"
+    }
+  }
+
+  func color() -> String {
+    switch self {
+      case .Spades, .Clubs:
+        return "black"
+      case .Hearts, .Diamonds:
+        return "red"
+    }
+  }
+}
+
+let hearts = Suit.Hearts
+let heartsDescription = hearts.simpleDescription()
+let heartsColor = hearts.color()
+
+println(heartsDescription)
+println(heartsColor)
+
+struct Card {
+  var rank: Rank
+  var suit: Suit
+
+  func simpleDescription() -> String {
+    return "The \(rank.simpleDescription()) of \(suit.simpleDescription())"
+  }
+}
+
+let threeOfSpades = Card(rank: .Three, suit: .Spades)
+let threeOfSpadesDescription = threeOfSpades.simpleDescription()
+
+println(threeOfSpades)
+println(threeOfSpadesDescription)
